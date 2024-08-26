656c4cf31f3e81897e895f7863aad5b4d783620f
MasterMagicStep <niklepa@mail.ru>
2024-08-26 11:05:11 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+#из любопытства проверка разных имен к 1 пeременной
+x = '12'
+CountOfHm = '12'
+Count_of_Hm = '12'
+print(id(x))
+print(id(CountOfHm))
+print(id(Count_of_Hm))
+#
+Hours_waste = '1.5'
+a ='1.5'
+#
+C_N = 'LittleBig Snake'
+#
+print('Курс: ',C_N,', всего задач: ' ,Count_of_Hm ,', затрачено часов: ',Hours_waste, ', среднее время выполнения:' ,float(a)/float(x),"часа")
